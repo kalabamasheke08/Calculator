dcb6d60ca25e5388d3fb63d91f33c4f0f2abff8e
snx2 <lunguj665@gmail.com>
2025-11-04 23:42:05 +0200

    connected the divide and multiply to the cli

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-#the entry files
 import typer
 from rich import print
+import utils.operators as operators  # <- clearer alias
 
 app = typer.Typer()
 
 @app.command()
-def version(version: str):
-    print(f"xyrculator version [bold red]pre:[green]0.1")
+def version():
+    print("xyrculator version [bold red]pre:[green]0.1")
 
-
+@app.command()
+def multiply(a: int, b: int):
+    print(operators.multiply(a, b))
+@app.command()
+def divide(a: int, b:int):
+    print(operators.divide(a,b))
 if __name__ == "__main__":
     app()
